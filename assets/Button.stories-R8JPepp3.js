30f82895275cb0d356a94d538bc8536003f0ba91
Loretta <wowshifter@gmail.com>
2025-02-02 10:49:09 +0200

    Updates

---
+++ assets/Button.stories-R8JPepp3.js
@@ -0,0 +1,60 @@
+import{j as U}from"./jsx-runtime-CLpGMVip.js";import{fn as V}from"./index-BZuLDMVQ.js";import{B as X}from"./Button-B2V25dYt.js";import{r as p}from"./index-B-SYruCi.js";function Y({title:u,titleId:m,...N},Q){return p.createElement("svg",Object.assign({xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",strokeWidth:1.5,stroke:"currentColor","aria-hidden":"true","data-slot":"icon",ref:Q,"aria-labelledby":m},N),u?p.createElement("title",{id:m},u):null,p.createElement("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"}))}const $=p.forwardRef(Y),te={title:"Example/Button",component:X,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"},textColor:{control:"color"},type:{control:"select",options:["primary","default","dashed","text","link"]},shape:{control:"select",options:["default","circle","round"]},size:{control:"select",options:["small","middle","large"]},loading:{control:"boolean"},disabled:{control:"boolean"},block:{control:"boolean"},danger:{control:"boolean"},ghost:{control:"boolean"},gradient:{control:"boolean"},iconPosition:{control:"select",options:["start","end"]}},args:{onClick:V()}},e={args:{type:"primary",label:"Primary Button"}},r={args:{type:"default",label:"Default Button"}},a={args:{type:"dashed",label:"Dashed Button"}},o={args:{type:"text",label:"Text Button"}},t={args:{type:"link",label:"Link Button",href:"https://example.com",target:"_blank"}},n={args:{danger:!0,type:"primary",label:"Danger Button"}},s={args:{disabled:!0,label:"Disabled Button"}},l={args:{loading:!0,label:"Loading Button"}},c={args:{block:!0,label:"Block Button"}},i={args:{gradient:!0,label:"Gradient Button"}},d={args:{label:"Search",icon:U.jsx($,{}),iconPosition:"start"}};var g,b,B;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    type: 'primary',
+    label: 'Primary Button'
+  }
+}`,...(B=(b=e.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var y,f,k;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    type: 'default',
+    label: 'Default Button'
+  }
+}`,...(k=(f=r.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var h,x,D;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    type: 'dashed',
+    label: 'Dashed Button'
+  }
+}`,...(D=(x=a.parameters)==null?void 0:x.docs)==null?void 0:D.source}}};var S,L,w;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    type: 'text',
+    label: 'Text Button'
+  }
+}`,...(w=(L=o.parameters)==null?void 0:L.docs)==null?void 0:w.source}}};var E,P,G;t.parameters={...t.parameters,docs:{...(E=t.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    type: 'link',
+    label: 'Link Button',
+    href: 'https://example.com',
+    target: '_blank'
+  }
+}`,...(G=(P=t.parameters)==null?void 0:P.docs)==null?void 0:G.source}}};var j,T,C;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    danger: true,
+    type: 'primary',
+    label: 'Danger Button'
+  }
+}`,...(C=(T=n.parameters)==null?void 0:T.docs)==null?void 0:C.source}}};var _,v,I;s.parameters={...s.parameters,docs:{...(_=s.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    label: 'Disabled Button'
+  }
+}`,...(I=(v=s.parameters)==null?void 0:v.docs)==null?void 0:I.source}}};var R,M,O;l.parameters={...l.parameters,docs:{...(R=l.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    loading: true,
+    label: 'Loading Button'
+  }
+}`,...(O=(M=l.parameters)==null?void 0:M.docs)==null?void 0:O.source}}};var z,A,F;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    block: true,
+    label: 'Block Button'
+  }
+}`,...(F=(A=c.parameters)==null?void 0:A.docs)==null?void 0:F.source}}};var W,Z,q;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    gradient: true,
+    label: 'Gradient Button'
+  }
+}`,...(q=(Z=i.parameters)==null?void 0:Z.docs)==null?void 0:q.source}}};var H,J,K;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    label: 'Search',
+    icon: <MagnifyingGlassIcon />,
+    iconPosition: 'start'
+  }
+}`,...(K=(J=d.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const ne=["Primary","Default","Dashed","Text","Link","Danger","Disabled","Loading","Block","Gradient","Icon"];export{c as Block,n as Danger,a as Dashed,r as Default,s as Disabled,i as Gradient,d as Icon,t as Link,l as Loading,e as Primary,o as Text,ne as __namedExportsOrder,te as default};
